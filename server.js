ecdcd91a915a8d551e47df5cafd53df5d8c47447
Behnam <shahriaribehnam@gmail.com>
2022-07-15 22:20:18 +0430

    Auth is implemented

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+var cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "Behnam?70",
+    database: "mail_box",
+  },
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt, saltRounds)
+);
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
